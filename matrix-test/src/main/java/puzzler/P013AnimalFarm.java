49f9f3d294e9b6e37f94a26cc416b9f58e2b25c5
matrix-stone <576729125@qq.com>
2016-01-21 17:39:36 +0800

    aa

---
+++ matrix-test/src/main/java/puzzler/P013AnimalFarm.java
@@ -0,0 +1,19 @@
+package puzzler;
+
+/**
+ * Puzzle 13: Animal Farm
+ * 
+ * @author matrix
+ *
+ * @date 2016年1月21日
+ */
+public class P013AnimalFarm {
+
+	public static void main(String[] args) {
+		String pig = "length: 10";
+		String dog = "length: " + pig.length();
+		System.out.println(dog);
+		System.out.println("Animals are equal: " + pig == dog);
+
+	}
+}
