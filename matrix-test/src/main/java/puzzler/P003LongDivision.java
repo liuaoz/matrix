a2e30ffd7ede3c9448fef63e96d07b9d1116f522
matrix-stone <576729125@qq.com>
2016-01-21 17:02:49 +0800

    aa:wq!

--- matrix-test/src/main/java/puzzler/P003LongDivision.java
+++ matrix-test/src/main/java/puzzler/P003LongDivision.java
@@ -3,6 +3,8 @@ package puzzler;
 /**
  * Puzzle 3:LongDivision</br>
  * 
+ * When working with large numbers, watch out for overflow—it’s a silent killer.
+ * 
  * @author matrix
  *
  * @date 2016年1月21日
@@ -11,7 +13,7 @@ public class P003LongDivision {
 	public static void main(String[] args) {
 		final long MICROS_PER_DAY = 24 * 60 * 60 * 1000 * 1000;// overflow
 		final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
-		System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);
+		System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);// 5
 
 	}
 }
